d1d1893a3252e38dd9ddc1f12256ffea723c0f1c
Thisaru Withanachchi <thisaruwithana@gmail.com>
2024-01-13 13:21:54 +0200

    Exercise 08 completed

--- e08/E08.java
+++ e08/E08.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 class E08 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number (0 to quit):");
+        int input = scanner.nextInt();
 
+        if(input == 0){
+            System.exit(0);
+        }else{
+            if(input % 2 == 0){
+                System.out.println( "Even number");
+            }else{
+                System.out.println("Odd number");
+            }
+        }
     }
 }

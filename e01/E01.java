f4c1662adf8a0e56dc60ea00e49da02a9753386c
Thisaru Withanachchi <thisaruwithana@gmail.com>
2024-01-13 10:56:51 +0200

    Exercise 01 completed

--- e01/E01.java
+++ e01/E01.java
@@ -1,5 +1,22 @@
 class E01 {
     public static void main(String[] args) {
 
+        int x = 10;
+        float y = 5.5f;
+        boolean flag = true;
+        char letter = 'a';
+        short year = 2022;
+        long population = 7000000000L;
+        byte num = 100;
+        double pi = 3.13457599923384753929348; 
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(flag);
+        System.out.println(letter);
+        System.out.println(year);
+        System.out.println(population);
+        System.out.println(num);
+        System.out.println(pi);
     }
 }

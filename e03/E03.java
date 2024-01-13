9a88947425d96123211fea75a6a2d5feb3ffa68d
Thisaru Withanachchi <thisaruwithana@gmail.com>
2024-01-13 14:35:59 +0200

    Exercise 03 correction 02

--- e03/E03.java
+++ e03/E03.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class E03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number:");
+        System.out.println("Enter a number: ");
         int input = scanner.nextInt();
 
         if(input > 0){

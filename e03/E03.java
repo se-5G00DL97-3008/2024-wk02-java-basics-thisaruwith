c2f27699d186f893cf5dc530d65e901d07199baa
Thisaru Withanachchi <thisaruwithana@gmail.com>
2024-01-13 12:06:42 +0200

    Exercise 03 completed

--- e03/E03.java
+++ e03/E03.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 class E03 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int input = scanner.nextInt();
 
+        if(input > 0){
+            System.out.println("Positive number");
+        }
+        if(input < 0){
+            System.out.println("Negative number");
+        }
     }
 }

ac34d2ef14b0f0049f4571b06a6f6fd0c22479ad
Thisaru Withanachchi <thisaruwithana@gmail.com>
2024-01-13 12:52:23 +0200

    Exercise 05 & 06 completed

--- e05/E05.java
+++ e05/E05.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 class E05 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int input = scanner.nextInt();
 
+        if(input == 1){
+            System.out.println( "Monnday");
+        }else if(input == 2){
+            System.out.println( "Tuesday");
+        }else if(input == 3){
+            System.out.println( "Wednesday");
+        }else if(input == 4){
+            System.out.println( "Thursday");
+        }else if(input == 5){
+            System.out.println( "Friday");
+        }else if(input == 6){
+            System.out.println( "Saturday");
+        }else if(input == 7){
+            System.out.println( "Sunday");
+        }else{
+            System.out.println( "Only 7 days in a week");
+        }
     }
 }
--- e06/E06.java
+++ e06/E06.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 class E06 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which multiplication table to show?");
+        int input = scanner.nextInt();
 
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i + " x " + input + " = " + (i * input));
+        }
     }
 }

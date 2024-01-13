49eff71b08040064b13807046729508eaf90b3e4
Thisaru Withanachchi <thisaruwithana@gmail.com>
2024-01-13 13:38:32 +0200

    Exercise 08 correction 02

--- e08/E08.java
+++ e08/E08.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 class E08 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number (0 to quit):");
-        int input = scanner.nextInt();
 
-        if(input == 0){
-            System.exit(0);
-        }else{
-            if(input % 2 == 0){
-                System.out.println( "Number is even");
-            }else{
-                System.out.println("Number is odd");
+        boolean valid = true;
+        while (valid) {
+            System.out.println("Enter a number (0 to quit):");
+            int input = scanner.nextInt();
+
+            if (input == 0) {
+                valid = false;
+            } else {
+                if (input % 2 == 0) {
+                    System.out.println("Number is even");
+                } else {
+                    System.out.println("Number is odd");
+                }
             }
         }
+
     }
 }

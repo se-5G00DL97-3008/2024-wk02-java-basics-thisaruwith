e82036cce508d2b110f65dd5bba4ccaba1c44204
Thisaru Withanachchi <thisaruwithana@gmail.com>
2024-01-13 12:11:40 +0200

    Exercise 04 completed

--- e04/E04.java
+++ e04/E04.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 class E04 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a day:");
+        int day = scanner.nextInt();
+        System.out.println("Enter a month:");
+        int month = scanner.nextInt();
 
+        if((day == 24) && (month == 12)){
+            System.out.println("Merry Cristmas");
+        }
     }
 }

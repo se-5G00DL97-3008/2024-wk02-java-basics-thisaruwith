c512f4692104af4c9ce5a2b49fdddb0df3df33dc
Thisaru Withanachchi <thisaruwithana@gmail.com>
2024-01-13 14:43:51 +0200

    Exercise 07 correction 01

--- e07/E07.java
+++ e07/E07.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 class E07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Which multiplicand multiplication table to show?");
+        System.out.println("Which multiplication table to show?");
         int multiplicand = scanner.nextInt();
-        System.out.println("Until which multiplyer?");
+        System.out.println("Until which multiplier should the table show?");
         int multiplyer = scanner.nextInt();
 
         for (int i = 0; i <= multiplyer; i++) {

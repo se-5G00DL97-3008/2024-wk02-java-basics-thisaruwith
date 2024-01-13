f6c851c68c99a954046c2596ed0fd542351d443b
Thisaru Withanachchi <thisaruwithana@gmail.com>
2024-01-13 13:50:46 +0200

    Exercise 09 completed

--- e09/E09.java
+++ e09/E09.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 class E09 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        boolean valid = true;
+        double total = 0;
+        double avg = 0.0;
+        int x = 1;
+        while (valid) {
+            System.out.println("Give a test score (-1 to quit): ");
+            double score = scanner.nextDouble();
+            
+            if (score == -1) {
+                System.exit(0);
+            }else{
+                total += score;
+                avg = total/x;
+            } 
+            System.out.println("Average: "+avg);
+            x++;           
+        }
     }
 }

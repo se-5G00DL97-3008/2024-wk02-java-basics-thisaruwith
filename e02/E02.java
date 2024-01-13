2e42891b4120cb0bb61308c9b786c4af2fb96904
Thisaru Withanachchi <thisaruwithana@gmail.com>
2024-01-13 11:38:38 +0200

    Exercise 02 completed

--- e02/E02.java
+++ e02/E02.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 class E02 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a number:");
+        int input1 = scanner.nextInt();
+        System.out.println("Give another number:");
+        int input2 = scanner.nextInt();
 
+        System.out.println(input1+" + "+input2+" = "+(input1+input2));
+        System.out.println(input1+" * "+input2+" = "+(input1*input2));
+        System.out.println(input1+" - "+input2+" = "+(input1-input2));
+        System.out.println(input1+" / "+input2+" = "+(input1/input2));
     }
 }

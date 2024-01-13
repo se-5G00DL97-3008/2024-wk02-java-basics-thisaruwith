c31513d6b2526d5a7802bd65ed435e7087109727
Thisaru Withanachchi <thisaruwithana@gmail.com>
2024-01-13 13:03:15 +0200

    Exercise 07 completed

--- e07/E07.java
+++ e07/E07.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 class E07 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which multiplicand multiplication table to show?");
+        int multiplicand = scanner.nextInt();
+        System.out.println("Until which multiplyer?");
+        int multiplyer = scanner.nextInt();
 
+        for (int i = 1; i <= multiplyer; i++) {
+            System.out.println(i + " x " + multiplicand + " = " + (i * multiplicand));
+        }
     }
 }

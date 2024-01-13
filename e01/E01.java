812a7226780cc2514675c7992f0a154a9ca29a1b
Thisaru Withanachchi <thisaruwithana@gmail.com>
2024-01-13 11:31:12 +0200

    Exercise 01 corrections 4

--- e01/E01.java
+++ e01/E01.java
@@ -4,12 +4,13 @@ class E01 {
         int x = 10;
         float y = 5.5f;
         boolean flag = true;
-        char letter = 'C';
+        char letter = 'c';
         short year = 2022;
         long population = 7000000000L;
         byte num = 100;
         double pi = 3.13457599923384753929348; 
 
+        System.out.println( "// Expected output");
         System.out.println(x);
         System.out.println(y);
         System.out.println(flag);

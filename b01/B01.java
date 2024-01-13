5d2e1e7a4739ec12b9ddf0c325b24755c83e8e0b
Thisaru Withanachchi <thisaruwithana@gmail.com>
2024-01-13 14:17:35 +0200

    Exercise B01 completed

--- b01/B01.java
+++ b01/B01.java
@@ -1,5 +1,15 @@
 class B01 {
     public static void main(String[] args) {
 
+        for (int i = 99; i >= 1; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            if (i != 1) {
+                System.out.println("Take one down and pass it around, " + (i - 1) + " bottles of beer on the wall.");
+            } else {
+                System.out.println("Take one down and pass it around, no more bottles of beer on the wall.");
+            }
+        }
+        System.out.println("No more bottles of beer on the wall, no more bottles of beer.");
+        System.out.println("Go to the store and buy some more, 99 bottles of beer on the wall.");
     }
 }
